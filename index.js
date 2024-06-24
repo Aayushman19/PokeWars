3a0aa049d4d6424cbf22f80aea06c973a0b7941f
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-06-25 01:37:32 +0530

    "Adding event action model"

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-import express from "express";
-
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) =>{
-    //Get request
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
-
-
-async function getPokemon (){
-    try{
-        const pokemonName = document.getElementById("p1").value.toLowerCase;
+async function getPokemon() {
+    try {
+        const pokemonName = document.getElementById("player1").value.toLowerCase;
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error("You have selected an unavailable pokemon");
         }
 
         const data = await response.json();
+        console.log(data);
         // get a random stat to compare
-        document.getElementsByTagName(r1_desc).innerHTML = data.height;
-        const stat2 = data.abilities[base_experience]
-        const stat3 = data.weight
+        btn1.addEventListner('click', function (e) {
+            document.getElementById("r1_desc1").innerHTML = data.height;
+            document.getElementById("r1_desc2").innerHTML = data.abilities[base_experience];
+            document.getElementById("r1_desc3").innerHTML = data.weight;
+        });
 
-    } catch(error){
+    }
+    catch (error) {
         console.log(error);
     }
 }
