06515ca39f0d325efaa61568ff2c5ed272d95784
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-06-27 00:32:45 +0530

    "fixed API data fetch issue"

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 async function getPokemon() {
     try {
-        const pokemonName = document.getElementById("player1").value.toLowerCase;
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        let pokemonName = document.getElementById("player1").value;
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName);
 
         if (!response.ok) {
             throw new Error("You have selected an unavailable pokemon");
         }
 
         const data = await response.json();
-        console.log(data);
-        // get a random stat to compare
-        btn1.addEventListner('click', function (e) {
+        // console.log(data);
+        // console.log(pokemonName);
+        var test = document.getElementsByClassName("button");
+        console.log(test);
+
+        test[0].addEventListner('click', function () {
             document.getElementById("r1_desc1").innerHTML = data.height;
             document.getElementById("r1_desc2").innerHTML = data.abilities[base_experience];
             document.getElementById("r1_desc3").innerHTML = data.weight;
         });
 
+        
     }
     catch (error) {
         console.log(error);
