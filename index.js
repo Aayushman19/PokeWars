91a5006a6a25da580c15c21581dbb48287cccef4
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-06-28 10:59:44 +0530

    "pokemon data fetched and displayed"

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 async function getPokemon() {
     try {
-        let pokemonName = document.getElementById("player1").value;
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName);
+        let pokemonName1 = document.getElementById("player1").value;
+        const response1 = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName1);
 
-        if (!response.ok) {
+        let pokemonName2 = document.getElementById("player2").value;
+        const response2 = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName2);
+
+        if (!response1.ok || !response2.ok) {
             throw new Error("You have selected an unavailable pokemon");
         }
 
-        const data = await response.json();
-        // console.log(data);
-        // console.log(pokemonName);
-        var test = document.getElementsByClassName("button");
-        console.log(test);
+        const data1 = await response1.json();
+        var plr1 = document.getElementById("btn1");
 
-        test[0].addEventListner('click', function () {
-            document.getElementById("r1_desc1").innerHTML = data.height;
-            document.getElementById("r1_desc2").innerHTML = data.abilities[base_experience];
-            document.getElementById("r1_desc3").innerHTML = data.weight;
-        });
+        plr1.addEventListener('click', setVal);
 
+        function setVal(){
+            document.getElementById("r1_desc1").innerHTML = data1.height;
+            document.getElementById("r1_desc2").innerHTML = data1.base_experience;
+            document.getElementById("r1_desc3").innerHTML = data1.weight;
+        }
         
+        const data2 = await response2.json();
+        var plr2 = document.getElementById("btn2");
+
+        plr2.addEventListener('click', setVal2);
+
+        function setVal2(){
+            document.getElementById("r2_desc1").innerHTML = data2.height;
+            document.getElementById("r2_desc2").innerHTML = data2.base_experience;
+            document.getElementById("r2_desc3").innerHTML = data2.weight;
+        }
     }
     catch (error) {
         console.log(error);
