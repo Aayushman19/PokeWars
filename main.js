2ec601e103161fa9ff32b9b01beb47abc48265b9
Aayushman Mishra <aayushmanmishra6@gmail.com>
2024-07-03 19:42:14 +0530

    created player chat, socket bug

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import pg from 'pg';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const db = new pg.Client({
     user: "postgres",
@@ -12,25 +14,33 @@ const db = new pg.Client({
 db.connect();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 5000;
+
+io.on("connection", (socket) => {
+    socket.on("user-message", (message) => {
+        io.emit("message", message);
+    });
+});
 
 app.use(express.static(path.resolve("./views")));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    return res.render("/views/index.html");
+    return res.sendFile("/views/index.html");
 });
 
 app.get('/', async (req, res) => {
     const winner = page.getElementById('result').innerText;
     console.log(winner);
-    try{
+    try {
         await db.query("insert into win_percentage (winner_name) values ($1)", [winner]);
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
